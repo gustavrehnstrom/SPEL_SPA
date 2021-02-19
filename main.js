b5e3ffde1155a13d1d2bbaef1e3440811feea655
Gustav Rehnström <gustav.rehnstrom@elev.ga.ntig.se>
2021-02-19 18:24:11 +0100

    början på js kod, koppla bilderna till artiklar

---
+++ main.js
@@ -0,0 +1,19 @@
+function showCSGO (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#csgo").classList.toggle("hidden");
+}
+
+function goBackCSGO (menu) {
+    document.querySelector("#csgo").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
+
+function showBO2 (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#bo2").classList.toggle("hidden");
+}
+
+function goBackBO2 (menu) {
+    document.querySelector("#bo2").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
