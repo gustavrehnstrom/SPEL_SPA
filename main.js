4137139627bf17ddae201e815bc0c642a6c5207d
Gustav Rehnström <gustav.rehnstrom@elev.ga.ntig.se>
2021-03-08 15:55:32 +0100

    fortsatt tillägning av js, koppla sidor till start

--- main.js
+++ main.js
@@ -17,3 +17,43 @@ function goBackBO2 (menu) {
     document.querySelector("#bo2").classList.toggle("hidden")
     document.querySelector(".Spel").classList.toggle("hidden")
 }
+
+function showBO3 (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#bo3").classList.toggle("hidden");
+}
+
+function goBackBO3 (menu) {
+    document.querySelector("#bo3").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
+
+function showBF2 (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#bf2").classList.toggle("hidden");
+}
+
+function goBackBF2 (menu) {
+    document.querySelector("#bf2").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
+
+function showMINE (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#mine").classList.toggle("hidden");
+}
+
+function goBackMINE (menu) {
+    document.querySelector("#mine").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
+
+function showGTA (game) {
+    document.querySelector(".Spel").classList.toggle("hidden");
+    document.querySelector("#gta").classList.toggle("hidden");
+}
+
+function goBackGTA (menu) {
+    document.querySelector("#gta").classList.toggle("hidden")
+    document.querySelector(".Spel").classList.toggle("hidden")
+}
